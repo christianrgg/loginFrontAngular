8187c6b8634d9d0fc7afa65212260337c08e3e96
Christian Roberto <christian.roberto.g@gmail.com>
2023-09-24 19:48:58 -0600

    Optimización: No te repitas a ti mismo - DRY

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import {environment} from "../../../environments/environments";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {catchError, map, Observable, of, tap, throwError} from "rxjs";
 import {AuthStatus, LoginResponse, User} from "../interfaces";
-import {CheckTokenResponse} from "../interfaces/check-token.response";
+import {CheckTokenResponse} from "../interfaces";
 
 
 @Injectable({
@@ -21,47 +21,37 @@ export class AuthService {
   public authStatus = computed(()=> this._authStatus())
 
   constructor() { }
+  // 32. Crear la funcion de optimización
+  private setAuthentication(user:User, token: string):boolean{
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+    return true
+  }
+
 
   login(email:string, password:string):Observable<boolean>{
-
-
     const url = `${this.baseUrl}/auth/login`;
     const body = {email, password};
 
     return this.http.post<LoginResponse>(url, body)
       .pipe(
-        tap(({user,token})=> {
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.authenticated);
-          localStorage.setItem('token', token);
-          console.log({user,token})
-        }),
-        map(()=>true),
+        map(({user,token})=> this.setAuthentication(user, token)),
         catchError(err => throwError(() => err.error.message))
       );
   }
 
-  // 27. Crear metodo para verificar token y al almacenarlo
   checkAuthStatus():Observable<boolean>{
     const url = `${this.baseUrl}/auth/check-token`;
     const token = localStorage.getItem('token');
-    if(!token) return of (false);
 
-    // 28. Para pasar los valores en los headers
+    if(!token) return of (false);
     const headers = new HttpHeaders()
       .set('Authorization', `Bearer ${token}`);
 
-    // 29. Retornar la petición hhtp con la interfaz correspondiente
-    // 31. Comienza en ".pipe" es que hacer con la respuesta
     return this.http.get<CheckTokenResponse>(url, {headers})
       .pipe(
-        map(({token, user})=>{
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.authenticated);
-          localStorage.setItem('token', token);
-          return true;
-        }),
-        //error
+        map(({user,token})=> this.setAuthentication(user, token)),
         catchError(()=> {
           this._authStatus.set(AuthStatus.notAuthenticated)
           return of(false)
